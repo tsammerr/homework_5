685e202e6b96cf4313fa75d0e9ba9fa81e8110a6
student <tarasovych.maria@gmail.com>
2022-10-01 13:41:18 +0300

    Update main.py ex 3

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 number_1 = int(input('1 number -> '))
 number_2 = int(input('2 number -> '))
 
-while number_2 >= number_1:
-    print(f"number = {number_2}")
-    number_2 -= 1
-if
+while number_1 <= number_2:
+    print(f"number = {number_1}")
+    number_2 += 1
+
+if number_1 % 7 != 0:
+    if number_1 < 7:
+        number_1 += (7 % number_1);
+else:
+    number_1 += (number_1 % 7);
+while number_1 <= number_2:
+        print(f'{number_1}', end=" ")
+        number_1 += 7
