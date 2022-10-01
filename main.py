edb28767b64c7b7362dd64c8a9a9b0ed663661f4
student <tarasovych.maria@gmail.com>
2022-10-01 14:07:20 +0300

    Update main.py ex 3 fix

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ number_1 = int(input('1 number -> '))
 number_2 = int(input('2 number -> '))
 
 while number_1 <= number_2:
-    print(f"number = {number_1}")
-    number_2 += 1
-
-if number_1 % 7 != 0:
-    if number_1 < 7:
-        number_1 += (7 % number_1);
-else:
-    number_1 += (number_1 % 7);
-while number_1 <= number_2:
-        print(f'{number_1}', end=" ")
-        number_1 += 7
+    print(f'{number_1} - ', end = "")
+    if number_1 % 3 == 0 and number_1 % 5 != 0:
+        print('Fizz')
+    elif number_1 % 5 == 0 and number_1 % 3 != 0:
+        print('Buzz')
+    elif number_1 % 3 == 0 and number_1 % 5 == 0:
+        print('Fizz Buzz')
+    else:
+        print(f'{number_1}')
+    number_1+=1
